c37344bb9527fad134bc377dc9dbbeeb82e72aa4
Delbert <richarddelbert84@gmail.com>
2020-05-10 04:04:38 +0700

    added env of how much data to be generated

--- database/seeds/ChosenWorkshopSeeder.php
+++ database/seeds/ChosenWorkshopSeeder.php
@@ -11,28 +11,31 @@ class ChosenWorkshopSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        User::create([
-            'name' => 'admin_cosmepick',
-            'email' => 'admin@cosmepick.com',
-            'phone' => '08123456789',
-            'website' => 'www.cosmpic.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'role' => 1,
-            'profile_pic_url' => 'profile_photo.png'
-            
-        ]);
+    {   
+        if(User::find(1) == null){
+            User::create([
+                'name' => 'admin_cosmepick',
+                'email' => 'admin@cosmepick.com',
+                'phone' => '08123456789',
+                'website' => 'www.cosmpic.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'role' => 1,
+                'profile_pic_url' => 'profile_photo.png'
+                
+            ]);
+    
+            User::create([
+                'name' => 'member_cosmepick',
+                'email' => 'member@cosmepick.com',
+                'phone' => '08123456789',
+                'website' => 'www.cosmepick.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'profile_pic_url' => 'profile_photo.png'
+                 // password
+            ]);
+        }
+       
 
-        User::create([
-            'name' => 'member_cosmepick',
-            'email' => 'member@cosmepick.com',
-            'phone' => '08123456789',
-            'website' => 'www.cosmepick.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'profile_pic_url' => 'profile_photo.png'
-             // password
-        ]);
-
-        factory(App\ChosenWorkshop::class, 25)->create();
+        factory(App\ChosenWorkshop::class,env('BANYAK_DATA',25))->create();
     }
 }
